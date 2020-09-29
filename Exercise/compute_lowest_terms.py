ebbf49fb4c8e2c901700bf035ceeaf68578f28a4
Abubakar Aliyu Badawi <abkr89@gmail.com>
2020-09-29 15:08:52 +0100

    passed test 7 and test 8

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -4,4 +4,21 @@ def lowest_terms(x):
 	x = x.split('/')
 	n = int(x[0])
 	d = int(x[1])
-	return "{}{}"
+	indication = ''
+	if n < 0 and d < 0:
+		indication = ''
+	elif n < 0 or d < 0:
+		indication = '-'
+	if n == 0:
+		return '0'
+	if d == 0:
+		return 'Undefined'
+	n = abs(n)
+	d = abs(d)
+	hcf = n
+	d_copy = d
+	while d_copy != 0:
+		i = d_copy
+		d_copy = hcf % d_copy
+		hcf = i
+	return "{}/{}".format(int(n), int(d))
