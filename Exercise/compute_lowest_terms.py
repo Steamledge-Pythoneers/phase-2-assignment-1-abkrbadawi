ac01a94d61621dae50a3e0007cfa36b45db16ab6
Abubakar Aliyu Badawi <abkr89@gmail.com>
2020-10-01 00:48:41 +0100

    create the lowest term function

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,24 +1,24 @@
 ## TODO: complete the function "lowest_terms" below
 
 def lowest_terms(x):
-	x = x.split('/')
+	x = x.split("/")
 	n = int(x[0])
 	d = int(x[1])
-	indication = ''
-	if n < 0 and d < 0:
-		indication = ''
-	elif n < 0 or d < 0:
-		indication = '-'
+	indication = ""
+	if n<0 and d<0:
+		indication = ""
+	elif n<0 or d<0:
+		indication = "-"
 	if n == 0:
-		return '0'
+		return "0"
 	if d == 0:
-		return 'Undefined'
+		return "Undefined"
 	n = abs(n)
 	d = abs(d)
 	hcf = n
 	d_copy = d
-	while d_copy != 0:
+	while d_copy !=0:
 		i = d_copy
-		d_copy = hcf % d_copy
+		d_copy = hcf%d_copy
 		hcf = i
-	return '{}{}/{}'.format(indication, int(n / hcf), int(d / hcf))
+	return "{}{}/{}".format(indication,int(n/hcf),int(d/hcf))
