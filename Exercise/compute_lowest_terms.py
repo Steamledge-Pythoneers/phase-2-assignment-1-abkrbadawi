2237aa08a87812cb410ae138820908dd730c7c61
Abubakar Aliyu Badawi <abkr89@gmail.com>
2020-10-01 01:21:21 +0100

    add docstring to the lowest_terms function

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,6 +1,16 @@
 ## TODO: complete the function "lowest_terms" below
 
 def lowest_terms(x):
+	"""This is a function that accept a string in form of a fraction, calculate and return the lowest
+	possible term of the string in form of a fraction.
+
+	input parameter:
+	x(string): a fraction in the form of numerator(n) and denominator(d)
+
+	 Return:
+	 	type: string
+	 	the lowest form of fraction in the form of indicatin, (n/hcf) ,(d/hcf)
+	 	"""
 	x = x.split("/")
 	n = int(x[0])
 	d = int(x[1])
